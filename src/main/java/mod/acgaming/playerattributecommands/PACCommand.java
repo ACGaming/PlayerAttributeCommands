46ed269df654426551c20f1f111c8aded2a31717
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-05-25 23:45:03 +0200

    Adjust command permission level

--- src/main/java/mod/acgaming/playerattributecommands/PACCommand.java
+++ src/main/java/mod/acgaming/playerattributecommands/PACCommand.java
@@ -63,6 +63,12 @@ public class PACCommand extends CommandBase
     }
 
     @Override
+    public int getRequiredPermissionLevel()
+    {
+        return 2;
+    }
+
+    @Override
     public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos)
     {
         if (args.length == 1)
